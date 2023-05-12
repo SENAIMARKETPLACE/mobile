080ca102bc415ec95cbc73e91d8a9a0f153e7c3b
Isabela Correia <isabela_2017@hotmail.com>
2023-05-11 21:57:17 -0300

    feat: Tela Login parte 2

--- lib/src/features/login/presentation/pages/screen_login.dart
+++ lib/src/features/login/presentation/pages/screen_login.dart
@@ -1,4 +1,8 @@
+import 'package:cep/src/core/utils/sollaris_text_button.dart';
+import 'package:cep/src/core/utils/sollaris_title.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../core/utils/sollaris_text_field.dart';
 
 class ScreenLogin extends StatefulWidget {
   const ScreenLogin({super.key});
@@ -10,74 +14,85 @@ class ScreenLogin extends StatefulWidget {
 class _ScreenLoginState extends State<ScreenLogin> {
   final _formKey = GlobalKey<FormState>();
   final _controllerEmail = TextEditingController();
+  final _controllerSenha = TextEditingController();
+  bool isExibe = true;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Stack(
-      alignment: AlignmentDirectional.center,
-      children: [
-        Image.asset(
-          'assets/image/banner.png',
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: double.infinity,
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width * 0.78,
-          height: MediaQuery.of(context).size.height * 0.70,
+      child: Scaffold(
+        body: Container(
+          // height: MediaQuery.of(context.size.height),
           decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(15),
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 171, 122, 214),
+              Color.fromARGB(255, 78, 232, 232)
+            ],
+          )),
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            // child: SingleChildScrollView(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/image/logo.png',
+                  width: 110,
+                  height: 110,
+                ),
+                const SollarisTitle(title: 'Login'),
+                const SizedBox(height: 20),
+                SollarisTextField(
+                    label: 'E-mail',
+                    hint: 'Informe seu e-mail',
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Valor inválido, digite novamente';
+                      }
+                      return null;
+                    },
+                    controller: _controllerEmail),
+                SollarisTextField(
+                    isExibPassword: isExibe,
+                    label: 'Senha',
+                    hint: 'Informe sua Senha',
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Valor inválido, digite novamente';
+                      }
+                      return null;
+                    },
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isExibe = !isExibe;
+                        });
+                      },
+                      icon: Icon(
+                          isExibe ? Icons.visibility : Icons.visibility_off),
+                    ),
+                    controller: _controllerSenha),
+                Expanded(
+                    child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SollarisTextButton(
+                      textButton: 'Acessar', function: () {}),
+                )),
+                // Expanded(
+                //   child: Align(
+                //   alignment: Alignment.bottomCenter,
+                //   child: SollarisTextButton(
+                //       textButton: 'Cadastro', function: () {}),
+                // )),
+              ],
             ),
           ),
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  width: 120,
-                  height: 120,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    image: DecorationImage(
-                      image: AssetImage('assets/image/logo.png'),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
-                  ),
-                  // child: Form(
-                  //     key: _formKey,
-                  //     child: SollarisTextField(
-                  //       label: 'Login',
-                  //       hint: 'Informe seu e-mail',
-                  //       validator: (value) {
-                  //         if (value != null) {
-                  //           return 'E-mail inválido. Digite novamente';
-                  //         }
-                  //         return null;
-                  //       },
-                  //       controller: _controllerEmail,
-                  //     )),
-                ),
-              )
-            ],
-          ),
         ),
-        // const SollarisTitle(
-        //   title: 'Login',
-        // )
-      ],
-    ));
+      ),
+      // ),
+    );
   }
 }
