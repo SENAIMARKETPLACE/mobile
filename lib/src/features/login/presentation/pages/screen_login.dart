0b36e6123c3bdaa9df7fc78a04b47cf5ac0be481
Flávio <flavio.romeiro20@outlook.com>
2023-05-18 22:36:11 -0300

    Feat: UX Login

--- lib/src/features/login/presentation/pages/screen_login.dart
+++ lib/src/features/login/presentation/pages/screen_login.dart
@@ -1,6 +1,6 @@
 import 'package:cep/src/core/utils/app_routes.dart';
+import 'package:cep/src/core/utils/sollaris_text_button.dart';
 import 'package:cep/src/core/utils/sollaris_text_field.dart';
-import 'package:cep/src/core/utils/sollaris_title.dart';
 import 'package:cep/src/features/login/presentation/bloc/login_bloc.dart';
 import 'package:cep/src/features/login/presentation/bloc/login_state.dart';
 import 'package:cep/src/dependency_assembly.dart' as di;
@@ -18,6 +18,8 @@ class _ScreenLoginState extends State<ScreenLogin> {
   final _formKey = GlobalKey<FormState>();
   final _controllerEmail = TextEditingController();
   final _controllerSenha = TextEditingController();
+  ImageProvider img = const AssetImage('assets/image/logo.png');
+
   bool isExibe = true;
 
   @override
@@ -33,67 +35,92 @@ class _ScreenLoginState extends State<ScreenLogin> {
                 .showSnackBar(SnackBar(content: Text(state.message)));
           }
         },
-        builder: (context, state) => Scaffold(
-          body: Container(
-            width: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'assets/image/logo.png',
-                  width: 110,
-                  height: 110,
-                ),
-                const SollarisTitle(title: 'Login'),
-                const SizedBox(height: 20),
-                SollarisTextField(
-                    label: 'E-mail',
-                    hint: 'Informe seu e-mail',
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Valor inválido, digite novamente';
-                      }
-                      return null;
-                    },
-                    controller: _controllerEmail),
-                SollarisTextField(
-                    isExibPassword: isExibe,
-                    label: 'Senha',
-                    hint: 'Informe sua Senha',
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Valor inválido, digite novamente';
-                      }
-                      return null;
-                    },
-                    // suffixIcon: IconButton(
-                    //   onPressed: () {
-                    //     context.read<LoginBloc>().add(
-                    //           LoginAccessEvent(
-                    //             credenciais: Login(
-                    //               email: _controllerLogin.text,
-                    //               password: _controllerSenha.text,
-                    //             ),
-                    //           ),
-                    //         );
-                    //   },
-                    //   child: const Text('Login'),
-                    // ),
-                    controller: _controllerSenha),
-                const Expanded(
-                    child: Align(
-                  alignment: Alignment.bottomCenter,
-                  // child: SollarisTextButton(
-                  //     textButton: 'Acessar', function: () {}),
-                )),
-                // Expanded(
-                //   child: Align(
-                //   alignment: Alignment.bottomCenter,
-                //   child: SollarisTextButton(
-                //       textButton: 'Cadastro', function: () {}),
-                // )),
-              ],
+        builder: (context, state) => SafeArea(
+          child: Scaffold(
+            body: Container(
+              width: MediaQuery.of(context).size.height,
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 198, 146, 243),
+                  Color.fromARGB(255, 167, 222, 222)
+                ],
+              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 160,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: img,
+                      ),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 86, 9, 153),
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SollarisTextField(
+                      label: 'E-mail',
+                      hint: 'Informe seu e-mail',
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Valor inválido, digite novamente';
+                        }
+                        return null;
+                      },
+                      controller: _controllerEmail),
+                  SollarisTextField(
+                      isExibPassword: isExibe,
+                      label: 'Senha',
+                      hint: 'Informe sua Senha',
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Valor inválido, digite novamente';
+                        }
+                        return null;
+                      },
+                      // suffixIcon: IconButton(
+                      //   onPressed: () {
+                      //     context.read<LoginBloc>().add(
+                      //           LoginAccessEvent(
+                      //             credenciais: Login(
+                      //               email: _controllerLogin.text,
+                      //               password: _controllerSenha.text,
+                      //             ),
+                      //           ),
+                      //         );
+                      //   },
+                      //   child: const Text('Login'),
+                      // ),
+                      controller: _controllerSenha),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SollarisTextButton(
+                      textButton: 'Acessar',
+                      function: () {},
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SollarisTextButton(
+                        textButton: 'Cadastro', function: () {}),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
